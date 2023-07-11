5366a058e12ec5ab63d12a09f0a67d40d1b7278d
xxMesseRxx <xxMesseRxx@HOME>
2023-07-11 20:13:51 +0500

    Were created 2 DAL: "CurrencyRate" and "RequeredRate".

---
+++ ExchangeRateLoader/DAL/CurrencyRate.cs
@@ -0,0 +1,23 @@
+﻿namespace ExchangeRateLoader.DAL;
+
+public class CurrencyRate
+{
+	public string BaseCurrency { get; }
+	public string Currency { get; }
+	public decimal SaleRateNB { get; }
+	public decimal PurchaseRateNB { get; }
+	public decimal? SaleRate { get; }
+	public decimal? PurchaseRate { get; }
+
+	public CurrencyRate(string baseCurrency, string currency,
+						decimal saleRateNB, decimal purchaseRateNB,
+						decimal? saleRate = null, decimal? purchaseRate = null)
+	{
+		BaseCurrency = baseCurrency;
+		Currency = currency;
+		SaleRateNB = saleRateNB;
+		PurchaseRateNB = purchaseRateNB;
+		SaleRate = saleRate;
+		PurchaseRate = purchaseRate;
+	}
+}
